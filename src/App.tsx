38653cf1244b81b18e37e128faf3eb08409b4ddd
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2020-04-26 21:43:08 +0800

    新增代理

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import Button from 'antd/es/button';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
-  render() {
+    componentDidMount(): void {
+        axios.defaults.baseURL = '/api'
+        axios({
+            method: "get",
+            url: '/test'
+        })
+    }
+
+    render() {
     return (
         <div className="App">
           <Button type="primary">Button</Button>
