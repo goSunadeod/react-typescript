95c85151d5d1f849c94bf569c8a46ee9f3ebe97a
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2020-10-30 20:28:15 +0800

    del

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-
-module.exports = function(app) {
-    app.use(createProxyMiddleware('/api', { target: 'https://demo.xiaoheiban.cn',  changeOrigin: true, }));
-};
